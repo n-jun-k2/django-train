a70fb396f2533706f798f387949f4d04e24ef74f
jun-pc <http.kj.com@gmail.com>
2021-01-08 14:45:54 +0900

    update shortcutsを利用したtemplate

--- src/project/poll/views.py
+++ src/project/poll/views.py
@@ -1,17 +1,15 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Question
 
 def index(request):
     print(Question.objects.all())
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('poll/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'poll/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
